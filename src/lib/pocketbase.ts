f0497f183f8d5d1448709190769c1428885dc36a
andre-brandao <brandaoandre00@gmail.com>
2023-08-13 14:40:15 -0300

    server transfer to AWS

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase';
 import { writable } from 'svelte/store';
 
-export const pb = new PocketBase('http://45.33.106.149'); 
+export const pb = new PocketBase('http://ec2-18-231-190-172.sa-east-1.compute.amazonaws.com:80/'); 
 
 export const currentUser = writable(pb.authStore.model);
 
